c4e2d6f5826654e7673ebffdd91cf8472017561a
kinkiss1 <gleb445@gmail.com>
2025-12-12 17:48:24 +0300

    Чтобы работало

--- src/ParthBE/Program.cs
+++ src/ParthBE/Program.cs
@@ -58,7 +58,9 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:3000")
+        policy.WithOrigins(
+                "http://localhost:3000",
+                "https://oop_frontend.skyvxl.ru")
               .AllowAnyMethod()
               .AllowAnyHeader()
               .AllowCredentials();
